34e6d88a68757c062e2b045fd46a5250d05a9c98
deanw77 <deanwaring77@outlook.com>
2023-11-28 20:08:09 +0000

    Question one now displaying oncestart is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,29 @@
 const startButton = document.querySelector("#start");
 const startScreen = document.querySelector("#start-screen");
-const questions = document.querySelector("#questions");
+const questionsList = document.querySelector("#questions");
+
+const questionTitle = document.querySelector("#question-title");
+const choices = document.querySelector("#choices");
 
 startButton.addEventListener("click", function() {
     startScreen.style.display = "none";
-    questions.style.display = "block";
+    questionsList.style.display = "block";
     startQuiz();
 });
 
 function startQuiz() {
-    
+    let counter = 0;
+    scrollQuestions(counter);
+    counter++
+}
+
+function scrollQuestions(counter) {
+    questionTitle.innerHTML = questions[counter].question;
+
+    let quizAnswers = '';
+    for (let i = 0; i < questions[counter].choices.length; i++) {
+        quizAnswers += '<button style="min-width: 120px">' + questions[counter].choices[i] + '</button>';
+    }
+
+    choices.innerHTML = quizAnswers;
 }
---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: 'Which of the following is NOT a valid keyword for a variable?',
+        choices: [ 'const', 'let', 'variable', 'var'],
+        correctAnswer: 'variable'    
+    },
+    {
+        question: 'How would you correctly generate a random number between 1 & 10 inclusive?',
+        choices: [ 
+            'math.random((math.floor() * 10)) + 1', 
+            'Math.random((Math.floor() * 10)) + 1', 
+            'math.floor((math.random() * 10)) + 1', 
+            'Math.floor((Math.random() * 10)) + 1'],
+        correctAnswer: 'Math.floor((Math.random() * 10)) + 1'    
+    }
+]
+
