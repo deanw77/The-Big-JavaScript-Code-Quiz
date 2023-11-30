df502d13197e3e8a5467d262ebaa50d82f18b15e
deanw77 <deanwaring77@outlook.com>
2023-11-30 08:39:52 +0000

    Game ends when all questions have been asked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,10 +9,10 @@ const badSound = document.querySelector("#incorrectAudio");
 const timer = document.querySelector("#time");
 const finalScore = document.querySelector("#final-score");
 
-
 let counter = 0;
 let secondsLeft = 60;
 let score = 0;
+let questionsCounter = 0;
 
 startButton.addEventListener("click", function() {
     startScreen.style.display = "none";
@@ -26,57 +26,61 @@ function startQuiz() {
 }
 
 function scrollQuestions(counter) {
-    questionTitle.innerHTML = questions[counter].question;
-
-    choices.textContent = '';
-    for (let i = 0; i < questions[counter].choices.length; i++) {
-        answrbtn = document.createElement('button');
-        answrbtn.setAttribute('id', 'answr'+i);
-        answrbtn.textContent = questions[counter].choices[i];
-        answrbtn.style.minWidth = '120px';
-        choices.appendChild(answrbtn);
-        if ( answrbtn.textContent === '') {
-            answrbtn.setAttribute("style", "display:none;");
+    if (questionsCounter < questions.length) {
+        questionTitle.innerHTML = questions[counter].question;
+        choices.textContent = '';
+        for (let i = 0; i < questions[counter].choices.length; i++) {
+            answrbtn = document.createElement('button');
+            answrbtn.setAttribute('id', 'answr'+i);
+            answrbtn.textContent = questions[counter].choices[i];
+            answrbtn.style.minWidth = '120px';
+            choices.appendChild(answrbtn);
+            if ( answrbtn.textContent === '') {
+                answrbtn.setAttribute("style", "display:none;");
+            }
         }
+
+        const answerOne = document.querySelector('#answr0');
+        const answerTwo = document.querySelector('#answr1');
+        const answerThree = document.querySelector('#answr2');
+        const answerFour = document.querySelector('#answr3');
+        const correctAnswer = questions[counter].correctAnswer;
+
+        answerOne.addEventListener("click", function() {
+            let text = answerOne.textContent;
+            if ( text === correctAnswer ) { nailedIt(); } 
+            else { unlucky(); }
+        });
+        answerTwo.addEventListener("click", function() {
+            let text = answerTwo.textContent;
+            if ( text === correctAnswer ) { nailedIt(); } 
+            else { unlucky(); }
+        });
+        answerThree.addEventListener("click", function() {
+            let text = answerThree.textContent;
+            if ( text === correctAnswer ) { nailedIt(); } 
+            else { unlucky(); }
+        });
+        answerFour.addEventListener("click", function() {
+            let text = answerFour.textContent;
+            if ( text === correctAnswer ) { nailedIt(); } 
+            else { unlucky(); }
+        });
+        questionsCounter += 1;  
+    } else {
+        endGame();
     }
-
-    const answerOne = document.querySelector('#answr0');
-    const answerTwo = document.querySelector('#answr1');
-    const answerThree = document.querySelector('#answr2');
-    const answerFour = document.querySelector('#answr3');
-    const correctAnswer = questions[counter].correctAnswer;
-
-    answerOne.addEventListener("click", function() {
-        let text = answerOne.textContent;
-        if ( text === correctAnswer ) { nailedIt(); } 
-        else { unlucky(); }
-    });
-    answerTwo.addEventListener("click", function() {
-        let text = answerTwo.textContent;
-        if ( text === correctAnswer ) { nailedIt(); } 
-        else { unlucky(); }
-    });
-    answerThree.addEventListener("click", function() {
-        let text = answerThree.textContent;
-        if ( text === correctAnswer ) { nailedIt(); } 
-        else { unlucky(); }
-    });
-    answerFour.addEventListener("click", function() {
-        let text = answerFour.textContent;
-        if ( text === correctAnswer ) { nailedIt(); } 
-        else { unlucky(); }
-    });
 }
 
 function nailedIt() {
-    //goodSound.play();
+    goodSound.play();
     counter++;
     scrollQuestions(counter);
     score = score + 5; 
 }
 
 function unlucky() {
-    //badSound.play();
+    badSound.play();
     secondsLeft = secondsLeft - 5;
 }
 
